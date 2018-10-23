53f36c7e5174f1b0fdb6178cf132720006152cf0
chenlei <294208016@qq.com>
2018-10-23 23:15:57 +0800

    修改使用ViewPager是隐藏ToolBar的ViewPager不显示的bug

--- materialdesigndemo/src/main/java/com/chenlei/materialdesigndemo/bottom_navigation_view/MainActivity.java
+++ materialdesigndemo/src/main/java/com/chenlei/materialdesigndemo/bottom_navigation_view/MainActivity.java
@@ -3,17 +3,17 @@ package com.chenlei.materialdesigndemo.bottom_navigation_view;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.chenlei.materialdesigndemo.R;
 import com.chenlei.materialdesigndemo.tab_layout.Fragment1;
-import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
@@ -26,9 +26,11 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     private NoScrollViewPager viewPager;
     private BottomNavigationView bnv;
+    private NestedScrollView nestedScrollView;
 
     private List<Fragment> list;
     private BottomViewAdapter bottomViewAdapter;
+    private boolean isBottomShow=true;
 
     private MenuItem menuItem;
     private Toolbar mToolbar;
@@ -40,15 +42,21 @@ public class MainActivity extends AppCompatActivity {
         mToolbar=findViewById(R.id.tool_bar);
         setSupportActionBar(mToolbar);
 
-        initUI();
-        bottomViewAdapter=new BottomViewAdapter(getSupportFragmentManager(),list);
+        initFragment();
 
         bnv=findViewById(R.id.bottom_navigation_view);
         viewPager=findViewById(R.id.view_pager);
+        nestedScrollView=findViewById(R.id.nested_scroll_view);
+
+        bottomViewAdapter=new BottomViewAdapter(getSupportFragmentManager(),list);
         viewPager.setAdapter(bottomViewAdapter);
         viewPager.setOffscreenPageLimit(4); //设置ViewPager缓存个数
         //设置禁止ViewPager滑动
         viewPager.setScroll(false);
+        listener();
+    }
+
+    private void listener(){
         //监听
         bnv.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -102,9 +110,29 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+//        //监听
+//        nestedScrollView.setOnScrollChangeListener(new NestedScrollView.OnScrollChangeListener() {
+//            @Override
+//            public void onScrollChange(NestedScrollView v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+//                Log.e("Test","22222");
+//                //上滑 并且 正在显示底部栏
+//                if (scrollY - oldScrollY > 0 && isBottomShow) {
+//                    isBottomShow = false;
+//                    //将Y属性变为底部栏高度  (相当于隐藏了)
+//                    Log.e("Test","00000");
+//                    bnv.animate().translationY(bnv.getHeight());
+//                } else {
+//                    if (scrollY - oldScrollY < 0 && !isBottomShow) {
+//                        Log.e("Test","11111");
+//                        isBottomShow = true;
+//                        bnv.animate().translationY(0);
+//                    }
+//                }
+//            }
+//        });
     }
 
-    private void initUI(){
+    private void initFragment(){
         list=new ArrayList<>();
         list.add(new Fragment1());
         list.add(new Fragment2());
