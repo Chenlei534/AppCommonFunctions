ff0b31ad589e15f44908f5365128c9d67f3814e1
chenlei <294208016@qq.com>
2018-10-26 18:50:29 +0800

    提交

--- glidedemo/src/main/java/com/chenlei/glidedemo/MainActivity.java
+++ glidedemo/src/main/java/com/chenlei/glidedemo/MainActivity.java
@@ -3,12 +3,23 @@ package com.chenlei.glidedemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String url = "http://img1.dzwww.com:8080/tupian_pl/20150813/16/7858995348613407436.jpg";
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mImageView=findViewById(R.id.image);
+        Glide.with(MainActivity.this)
+                .load(url)
+                .into(mImageView);
+
     }
 }
